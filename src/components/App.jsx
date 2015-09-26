bd9db9c73539161eab86d0680cccbdf01a44a420
fredy8 <fredy.altamirano8@gmail.com>
2015-09-26 16:54:21 -0500

    basic registration form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Registration from './Registration.jsx';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  componentDidMount() {
-  }
   render() {
-    return <div>Hello World!</div>
+    return <Registration></Registration>
   }
 }
 
