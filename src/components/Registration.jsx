afda5e4085550f9adf0cf832646954d6167bd974
fredy8 <fredy.altamirano8@gmail.com>
2015-09-26 16:56:44 -0500

    add missing files

---
+++ src/components/Registration.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfirmChange = this.handleConfirmChange.bind(this);
+    this.registerUser = this.registerUser.bind(this);
+  }
+  render() {
+    return (
+      <form>
+        <label>Email:</label>
+        <input className='form-control' type='text' value={this.state.email} onChange={this.handleEmailChange}></input>
+        <label>Password:</label>
+        <input className='form-control' type='text' value={this.state.password} onChange={this.handlePasswordChange}></input>
+        <label>Confirm:</label>
+        <input className='form-control' type='text' value={this.state.confirmPassword} onChange={this.handleConfirmChange}></input>
+        <button type='button' className='btn btn-success' type='button' onClick={this.registerUser}>Sign up</button>
+      </form>
+    );
+  }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+  handleConfirmChange(event) {
+    this.setState({ confirmPassword: event.target.value });
+  }
+  registerUser() {
+
+  }
+}
